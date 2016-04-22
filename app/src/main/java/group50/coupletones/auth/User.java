f0a8d5fde0008d0d5a8b60938d2faa42728ce8fd
Calclavia <admin@calclavia.com>
2016-04-22 13:17:17 -0700

    Created basic user class

---
+++ app/src/main/java/group50/coupletones/auth/User.java
@@ -0,0 +1,48 @@
+package group50.coupletones.auth;
+
+/**
+ * The user represents a single user in the app.
+ */
+public class User {
+    /**
+     * The id of the user. Immutable.
+     */
+    private final String id;
+
+    /**
+     * The name of the user. Immutable.
+     */
+    private final String name;
+
+    /**
+     * The email of the user. Immutable
+     */
+    private final String email;
+
+    /**
+     * Creates a new user with a given name and email.
+     *
+     * @param id    The idof the user
+     * @param name  The name of the user
+     * @param email The email of the user
+     */
+    public User(String id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    /**
+     * @return The name of the user
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @return The email of the user
+     */
+    public String getEmail() {
+        return email;
+    }
+}

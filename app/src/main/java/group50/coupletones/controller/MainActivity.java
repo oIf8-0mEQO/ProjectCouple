13be43510f946c970843653ba0c8c73f6997bec6
Calclavia <admin@calclavia.com>
2016-04-25 10:16:11 -0700

    Implemented dynamic fragment creation in MainActivity

--- app/src/main/java/group50/coupletones/controller/MainActivity.java
+++ app/src/main/java/group50/coupletones/controller/MainActivity.java
@@ -1,8 +1,8 @@
 package group50.coupletones.controller;
 
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
+import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -16,12 +16,12 @@ import group50.coupletones.controller.tab.FavoriteLocationsFragment;
  * tab page as a fragment and renders them accordingly.
  */
 public class MainActivity extends AppCompatActivity implements
-  FavoriteLocationsFragment.Listener {
+  FavoriteLocationsFragment.Listener, OnMenuTabClickListener {
 
   private static final String TAG = "MainActivity";
 
   /**
-   * The object that handles the bottom bar
+   * The bottom tab bar handler
    */
   private BottomBar mBottomBar;
 
@@ -30,38 +30,12 @@ public class MainActivity extends AppCompatActivity implements
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    mBottomBar = BottomBar.attach(this, savedInstanceState);
-    mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, new OnMenuTabClickListener() {
-      @Override
-      public void onMenuTabSelected(
-        @IdRes
-          int menuItemId) {
-        Log.d(TAG, "onMenuTabSelected: " + menuItemId);
-        switch (menuItemId) {
-          case R.id.partnerLocations:
-            break;
-          case R.id.favoriteLocations:
-            break;
-          case R.id.settings:
-            break;
-        }
-      }
+    // Create default fragment
+    Fragment defaultFragment = new FavoriteLocationsFragment.Factory().build();
+    setFragment(defaultFragment);
 
-      @Override
-      public void onMenuTabReSelected(
-        @IdRes
-          int menuItemId) {
-        Log.d(TAG, "onMenuTabReSelected: " + menuItemId);
-        switch (menuItemId) {
-          case R.id.partnerLocations:
-            break;
-          case R.id.favoriteLocations:
-            break;
-          case R.id.settings:
-            break;
-        }
-      }
-    });
+    mBottomBar = BottomBar.attach(this, savedInstanceState);
+    mBottomBar.setItemsFromMenu(R.menu.bottombar_menu, this);
 
     // Setting colors for different tabs when there's more than three of them.
     // You can set colors for tabs in three different ways as shown below.
@@ -72,11 +46,53 @@ public class MainActivity extends AppCompatActivity implements
   }
 
   @Override
+  public void onMenuTabSelected(
+    @IdRes
+      int menuItemId) {
+    Log.d(TAG, "onMenuTabSelected: " + menuItemId);
+    switch (menuItemId) {
+      case R.id.partnerLocations:
+        break;
+      case R.id.favoriteLocations:
+        break;
+      case R.id.settings:
+        break;
+    }
+  }
+
+  @Override
+  public void onMenuTabReSelected(
+    @IdRes
+      int menuItemId) {
+    Log.d(TAG, "onMenuTabReSelected: " + menuItemId);
+    switch (menuItemId) {
+      case R.id.partnerLocations:
+        break;
+      case R.id.favoriteLocations:
+        break;
+      case R.id.settings:
+        break;
+    }
+  }
+
+  @Override
   protected void onSaveInstanceState(Bundle outState) {
     super.onSaveInstanceState(outState);
 
     // Necessary to restore the BottomBar's state, otherwise we would
     // lose the current tab on orientation change.
     mBottomBar.onSaveInstanceState(outState);
+  }
+
+  /**
+   * Sets the content of the MainActivity with the given fragment
+   * @param fragment The fragment to set for the main content
+   */
+  private void setFragment(Fragment fragment) {
+    getSupportFragmentManager()
+      .beginTransaction()
+      .replace(R.id.fragment_container, fragment) // Replace whatever is in the fragment_container view
+      .addToBackStack(null) // Add the transaction to the back stack if needed
+      .commit();  // Commit the transaction
   }
 }

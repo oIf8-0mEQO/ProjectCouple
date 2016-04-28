31deee02a2a1768ed1380b48db8323af5d84a264
Calclavia <admin@calclavia.com>
2016-04-27 23:03:41 -0700

    Login now displays when user is not logged in, and takes user to MainActivity if logged in

--- app/src/main/java/group50/coupletones/controller/LoginActivity.java
+++ app/src/main/java/group50/coupletones/controller/LoginActivity.java
@@ -11,11 +11,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
-import com.google.android.gms.common.ConnectionResult;
+import android.widget.Toast;
 import group50.coupletones.R;
 import group50.coupletones.auth.Authenticator;
 import group50.coupletones.auth.GoogleAuthenticator;
-import group50.coupletones.auth.GoogleUser;
+import group50.coupletones.auth.User;
 import group50.coupletones.util.Taggable;
 
 /**
@@ -23,7 +23,7 @@ import group50.coupletones.util.Taggable;
  */
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, Taggable {
 
-  private Authenticator<GoogleAuthenticator, GoogleUser, ConnectionResult> auth;
+  private Authenticator<GoogleAuthenticator, User, String> auth;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +33,24 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     // Create Google Authenticator for automatic sign in.
     auth = new GoogleAuthenticator(this);
     auth.autoSignIn();
+    auth.onSuccess(this::onUserLogin);
 
     TextView coupleTones_text = (TextView) findViewById(R.id.sign_in_button);
     Typeface pierSans = Typeface.createFromAsset(getAssets(), getString(R.string.pier_sans));
     coupleTones_text.setTypeface(pierSans);
     findViewById(R.id.sign_in_button).setOnClickListener(this);
+  }
+
+  private User onUserLogin(User user) {
+    //TODO: Better design to signify how the sign in failed. User should never be null?
+    if (user != null) {
+      Intent intent = new Intent(this, MainActivity.class);
+      startActivity(intent);
+    } else {
+      //TODO: Remove? For debug. Use string constant
+      Toast.makeText(this, "Please Login", Toast.LENGTH_SHORT).show();
+    }
+    return user;
   }
 
   @Override

af1dd8ca6fb9d7419eecd28ff7f39b6b9d021df0
JosephBCox <joseph.vive@gmail.com>
2016-04-27 17:55:27 -0800

    Implemented search method.

--- app/src/main/java/group50/coupletones/MapActivity.java
+++ app/src/main/java/group50/coupletones/MapActivity.java
@@ -1,6 +1,8 @@
 package group50.coupletones;
 
 import android.Manifest;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
@@ -18,6 +20,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -27,6 +30,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     private GoogleMap mMap;
     private List<FavoriteLocation> favLocations = new LinkedList<FavoriteLocation>();
+    private Geocoder geocoder = new Geocoder(this);
 
     LocationListener locationListener = new LocationListener() {
         @Override
@@ -35,7 +39,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
             {
                 if (distanceInMiles(i.getPosition(), new LatLng(location.getLatitude(), location.getLongitude())) < 0.1)
                 {
-                    SomeOtherClass.sendNotification(i);//THIS NEEDS TO BE CHANGED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    SomeOtherClass.sendNotification(i);//TODO: properly implement this method call
                 }
             }
         }
@@ -50,7 +54,7 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
     GoogleMap.OnMapClickListener clickListener = new GoogleMap.OnMapClickListener() {
         @Override
         public void onMapClick(LatLng latLng) {
-            String name = UIClass.inputName();//////THIS NEEDS TO BE CHANGED!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            String name = UIClass.inputName();//TODO: properly implement this method call
             favLocations.add(new FavoriteLocation(name, latLng));
             populateMap();
         }
@@ -69,12 +73,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
 
     /**
      * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
      */
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -83,13 +81,6 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
         String locationProvider = LocationManager.GPS_PROVIDER;
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                     100);
@@ -126,6 +117,21 @@ public class MapActivity extends FragmentActivity implements OnMapReadyCallback 
         double distKilometers = Math.sqrt(Math.pow(distLatKilometers, 2) + Math.pow(distLongKilometers, 2));
         double distMiles = 0.621371*distKilometers;
         return distMiles;
+    }
+
+
+    public void search(String nameLocation)
+    {
+        try
+        {
+            List<Address> locations = geocoder.getFromLocationName(nameLocation, 10);
+            Address pickedAddress = UIClass.displayLocations(locations);//TODO: properly implement this method
+            favLocations.add(new Location(pickedAddress.getAddressLine(0), new LatLong(pickedAddress.getLatitude(), pickedAddress.getLongitude())));
+        }
+        catch(IOException e)
+        {
+            //TODO: write exception handling code
+        }
     }
     
 }

ff992952078ab4adfbeaaa190567c2e35213bca2
Calclavia <admin@calclavia.com>
2016-04-27 22:12:32 -0700

    Rewrote authentication scheme using Strategy Pattern

--- app/src/main/java/group50/coupletones/controller/LoginActivity.java
+++ app/src/main/java/group50/coupletones/controller/LoginActivity.java
@@ -9,47 +9,33 @@ import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import com.google.android.gms.auth.api.Auth;
-import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import group50.coupletones.App;
 import group50.coupletones.R;
+import group50.coupletones.auth.Authenticator;
+import group50.coupletones.auth.GoogleAuthenticator;
 import group50.coupletones.auth.GoogleUser;
 
 /**
  * The LoginActivity controls the login page of the app.
  */
-public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, View.OnClickListener {
-
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
   /**
    * The tag used for this activity
    */
   private static final String TAG = "LoginActivity";
 
-  /**
-   * The request code for sign in intent
-   */
-  private static final int RC_SIGN_IN = 9001;
+  private Authenticator<GoogleAuthenticator, GoogleUser, ConnectionResult> auth;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_login);
 
-
-    //TODO: This code should be moved
-    // Build a GoogleApiClient with access to the Google Sign-In API and the
-    // options specified by gso.
-    App.instance().setGoogleApiClient(
-      new GoogleApiClient.Builder(this)
-        .enableAutoManage(this, this)
-        .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
-        .build()
-    );
+    // Create Google Authenticator for automatic sign in.
+    auth = new GoogleAuthenticator(this);
+    auth.autoSignIn();
 
     TextView coupleTones_text = (TextView) findViewById(R.id.sign_in_button);
     Typeface pierSans = Typeface.createFromAsset(getAssets(), getString(R.string.pier_sans));
@@ -62,52 +48,14 @@ public class LoginActivity extends AppCompatActivity implements GoogleApiClient.
     switch (v.getId()) {
       // When the sign in button is clicked
       case R.id.sign_in_button:
-        onSignIn();
+        auth.signIn();
         break;
     }
-  }
-
-  /**
-   * Called when the sign in button is clicked.
-   * <p>
-   * Opens an Android Intent and asks Google to sign in.
-   */
-  private void onSignIn() {
-    Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(App.instance().getGoogleApiClient());
-    startActivityForResult(signInIntent, RC_SIGN_IN);
-  }
-
-  @Override
-  public void onConnectionFailed(ConnectionResult connectionResult) {
-    //TODO: Improve err message
-    Log.e(TAG, "Failed to connect");
   }
 
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
-
-    // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
-    if (requestCode == RC_SIGN_IN) {
-      GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
-      handleSignInResult(result);
-    }
+    auth.onActivityResult(requestCode, resultCode, data);
   }
-
-  /**
-   * Handles the sign in request
-   *
-   * @param result The GoogleSignInResult
-   */
-  private void handleSignInResult(GoogleSignInResult result) {
-    Log.d(TAG, "handleSignInResult: " + result.isSuccess());
-    if (result.isSuccess()) {
-      // Signed in successfully, store authenticated user
-      App.instance().setLocalUser(new GoogleUser(result.getSignInAccount()));
-    } else {
-      // Signed out, show unauthenticated UI.
-      App.instance().setLocalUser(null);
-    }
-  }
-
 }

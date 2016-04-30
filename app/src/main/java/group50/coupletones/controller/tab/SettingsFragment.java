d7f21c6cbdee069d4f03764799fa5c6cbcfb4c7f
bdchi <bdchi@ucsd.edu>
2016-04-29 19:42:14 -0700

    Fixed Dynamic Strings for Settings Fragment

--- app/src/main/java/group50/coupletones/controller/tab/SettingsFragment.java
+++ app/src/main/java/group50/coupletones/controller/tab/SettingsFragment.java
@@ -8,9 +8,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 import org.w3c.dom.Text;
 
+import javax.inject.Inject;
+
 import group50.coupletones.R;
+import group50.coupletones.auth.GoogleUser;
 
 /**
  * A simple {@link Fragment} subclass for the Favorite Locations tab.
@@ -19,79 +24,88 @@ import group50.coupletones.R;
  */
 public class SettingsFragment extends TabFragment<SettingsFragment.Listener> {
 
-    public SettingsFragment() {
-        super(Listener.class);
-    }
+  public SettingsFragment() {
+    super(Listener.class);
+  }
 
-    /**
-     * Use this factory method to create a new instance of SettingsFragment.
-     */
-    public static SettingsFragment build() {
-        SettingsFragment fragment = new SettingsFragment();
-        Bundle args = new Bundle();
-        // TODO: Set arguments
-        fragment.setArguments(args);
-        return fragment;
-    }
+  @Inject
+  GoogleUser user;
 
-    @Override
-    protected int getResourceId() {
-        return R.layout.fragment_settings;
-    }
+  /**
+   * Use this factory method to create a new instance of SettingsFragment.
+   */
+  public static SettingsFragment build() {
+    SettingsFragment fragment = new SettingsFragment();
+    Bundle args = new Bundle();
+    // TODO: Set arguments
+    fragment.setArguments(args);
+    return fragment;
+  }
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            //TODO: Read arguments
-        }
-    }
+  @Override
+  protected int getResourceId() {
+    return R.layout.fragment_settings;
+  }
 
-    /**
-     * A method that sets the font of each textview on the Settings Fragment.
-     */
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.fragment_settings, container, false);
-        Typeface pierSans = Typeface.createFromAsset(getActivity().getAssets(),
-                getString(R.string.pier_sans));
-        TextView yourProfileText = (TextView) v.findViewById(R.id.my_profile_header);
-        TextView yourNameText = (TextView) v.findViewById(R.id.your_name_header);
-        TextView yourName = (TextView) v.findViewById(R.id.your_name);
-        TextView yourAccountText = (TextView) v.findViewById(R.id.your_account_header);
-        TextView yourAccount = (TextView) v.findViewById(R.id.your_email);
-        TextView partnersProfileText = (TextView) v.findViewById(R.id.partners_profile_text);
-        TextView partnerNameText = (TextView) v.findViewById(R.id.partner_name_header);
-        TextView partnerName = (TextView) v.findViewById(R.id.partner_name);
-        TextView partnerAccountText = (TextView) v.findViewById(R.id.partner_account_header);
-        TextView partnerAccount = (TextView) v.findViewById(R.id.partner_email);
-        TextView logoutButton = (TextView) v.findViewById(R.id.logout_button);
-        yourProfileText.setTypeface(pierSans);
-        yourNameText.setTypeface(pierSans);
-        yourName.setTypeface(pierSans);
-        yourAccountText.setTypeface(pierSans);
-        yourAccount.setTypeface(pierSans);
-        partnersProfileText.setTypeface(pierSans);
-        partnerNameText.setTypeface(pierSans);
-        partnerName.setTypeface(pierSans);
-        partnerAccountText.setTypeface(pierSans);
-        partnerAccount.setTypeface(pierSans);
-        logoutButton.setTypeface(pierSans);
-        return v;
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    if (getArguments() != null) {
+      //TODO: Read arguments
     }
+  }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface Listener {
-        // TODO: Fill with interface methods
-    }
+  /**
+   * A method that sets the font of each textview on the Settings Fragment.
+   */
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    View v = inflater.inflate(R.layout.fragment_settings, container, false);
+    Typeface pierSans = Typeface.createFromAsset(getActivity().getAssets(),
+      getString(R.string.pier_sans));
+
+    // User's Profile Cardview
+    TextView yourProfileText = (TextView) v.findViewById(R.id.my_profile_header);
+    TextView yourNameText = (TextView) v.findViewById(R.id.your_name_header);
+    String yourName = user.getId();
+    TextView yourAccountText = (TextView) v.findViewById(R.id.your_account_header);
+    String yourAccount = user.getEmail();
+
+    // Partner's Profile Cardview
+    // TODO: change partner's name/email to get keys from backend
+    TextView partnersProfileText = (TextView) v.findViewById(R.id.partners_profile_text);
+    TextView partnerNameText = (TextView) v.findViewById(R.id.partner_name_header);
+    TextView partnerName = (TextView) v.findViewById(R.id.partner_name);
+    TextView partnerAccountText = (TextView) v.findViewById(R.id.partner_account_header);
+    TextView partnerAccount = (TextView) v.findViewById(R.id.partner_email);
+
+    TextView logoutButton = (TextView) v.findViewById(R.id.logout_button);
+    yourProfileText.setTypeface(pierSans);
+    yourNameText.setTypeface(pierSans);
+    yourNameText.setText(yourName);
+    yourAccountText.setTypeface(pierSans);
+    yourAccountText.setText(yourAccount);
+    partnersProfileText.setTypeface(pierSans);
+    partnerNameText.setTypeface(pierSans);
+    partnerName.setTypeface(pierSans);
+    partnerAccountText.setTypeface(pierSans);
+    partnerAccount.setTypeface(pierSans);
+    logoutButton.setTypeface(pierSans);
+    return v;
+  }
+
+  /**
+   * This interface must be implemented by activities that contain this
+   * fragment to allow an interaction in this fragment to be communicated
+   * to the activity and potentially other fragments contained in that
+   * activity.
+   * <p>
+   * See the Android Training lesson <a href=
+   * "http://developer.android.com/training/basics/fragments/communicating.html"
+   * >Communicating with Other Fragments</a> for more information.
+   */
+  public interface Listener {
+    // TODO: Fill with interface methods
+  }
 }

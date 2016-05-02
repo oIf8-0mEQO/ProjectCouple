2cc8ec1ed150ae250b0166c6ae327db1929f4444
sharmaine <hello@sharmaine.me>
2016-05-01 23:09:38 -0700

    Refactored FaveLocation class that holds name, address, and icon information

--- app/src/main/java/group50/coupletones/FaveLocations.java
+++
@@ -1,9 +0,0 @@
-package group50.coupletones;
-
-/**
- * A class that creates a FaveLocations object that contains a favorite location name and address.
- */
-public class FaveLocations {
-    String locationName;
-    String locationAddress;
-}

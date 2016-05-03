edc42f4f0e7f13c475a533146e4120157f33ca52
Calclavia <admin@calclavia.com>
2016-05-02 17:03:06 -0700

    Added basic XMPP JS server

--- server/src/index.js
+++ server/src/index.js
@@ -1,23 +1,46 @@
-var gcm = require('node-gcm');
+/**
+ * @author Henry Mao
+ * @date 5/2/16.
+ */
 
+//var Notification = require('./notification');
+var GCM = require('node-gcm-ccs');
+
+var PROJECT_ID = "794558589013";
 var SERVER_API_KEY = "AIzaSyCyMy2k4cpGNsqqvcilg2GyFjXQSAZ_qbE";
 
-var message = new gcm.Message();
-message.addData('key1', 'msg1');
+var gcm = GCM(PROJECT_ID, SERVER_API_KEY);
 
-// Set up the sender with you API key
-var sender = new gcm.Sender(SERVER_API_KEY);
+// Log connection events
+gcm.on('connected', console.log);
+gcm.on('disconnected', console.log);
+gcm.on('online', console.log);
+gcm.on('error', console.log);
 
-var regTokens = ['YOUR_REG_TOKEN_HERE'];
-
-// Now the sender can be used to send messages
-sender.send(message, {registrationTokens: regTokens}, function (err, response) {
-	if (err) console.error(err);
-	else    console.log(response);
+/**
+ * Upstream called by client to the server when the client enters a location.
+ */
+gcm.on('message', (messageId, from, category, data) => {
+	console.log('received message', arguments);
 });
 
-// Send to a topic, with no retry this time
-sender.sendNoRetry(message, {topic: '/topics/global'}, function (err, response) {
-	if (err) console.error(err);
-	else    console.log(response);
-});
+gcm.send("some guy",
+	{message: 'hello world'},
+	{delivery_receipt_requested: true},
+	(err, messageId, to) => {
+		if (!err) {
+			console.log('sent message to', to, 'with message_id =', messageId);
+		} else {
+			console.log('failed to send message: ' + err);
+		}
+	}
+);
+
+/*
+ var n = new Notification(SERVER_API_KEY);
+
+ var message = new gcm.Message();
+ message.addData('key1', 'msg1');
+
+ n.send([], message);
+ */

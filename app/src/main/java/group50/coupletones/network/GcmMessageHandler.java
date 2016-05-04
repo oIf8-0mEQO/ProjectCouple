461cf20a541c31e803e5a0d2e0fe6dbbb7d03a9d
sharmaine <hello@sharmaine.me>
2016-05-04 15:20:16 -0700

    Added GcmMessageHandler class

---
+++ app/src/main/java/group50/coupletones/network/GcmMessageHandler.java
@@ -0,0 +1,58 @@
+
+package group50.coupletones.network;
+
+/**
+ * Created by Agneev on 22-04-2016.
+ */
+
+import com.google.android.gms.gcm.GcmReceiver;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
+
+public class GcmMessageHandler extends IntentService {
+
+    String msg;
+    private Handler handler;
+
+    public GcmMessageHandler() {
+        super("GcmMessageHandler");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        handler = new Handler();
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+
+        String messageType = gcm.getMessageType(intent);
+
+        msg = extras.getString("message");
+        showToast();
+
+        Log.i("GCM", "Received: (" + messageType + ") " + extras.getString("message"));
+
+        GcmReceiver.completeWakefulIntent(intent);
+    }
+
+    public void showToast() {
+        handler.post(new Runnable() {
+
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}

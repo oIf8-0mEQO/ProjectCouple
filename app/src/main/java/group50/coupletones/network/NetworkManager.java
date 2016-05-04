c5f12f5c98ebbce10640b9e25c8532c03be36283
sharmaine <hello@sharmaine.me>
2016-05-04 15:18:59 -0700

    Added NetworkManager interface

--- app/src/main/java/group50/coupletones/network/NetworkManager.java
+++ app/src/main/java/group50/coupletones/network/NetworkManager.java
@@ -5,7 +5,7 @@ import group50.coupletones.util.function.Function;
 /**
  * Created by sharmaine on 5/2/16.
  */
-public interface MessageNetwork {
+public interface NetworkManager {
     void send(String message);
     void onReceive(Function callBack);
 }

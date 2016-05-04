e6a414277aec0b588c691fcc51944449ce46b407
sharmaine <hello@sharmaine.me>
2016-05-04 16:37:32 -0700

    Added register function to NetworkManager interface

--- app/src/main/java/group50/coupletones/network/NetworkManager.java
+++ app/src/main/java/group50/coupletones/network/NetworkManager.java
@@ -1,13 +1,18 @@
 package group50.coupletones.network;
 
+import android.content.Context;
 import android.os.AsyncTask;
 
 import group50.coupletones.util.function.Function;
 
 /**
- * Created by sharmaine on 5/2/16.
+ * @author sharmaine
+ * @since 5/2/16
  */
 public interface NetworkManager {
   AsyncTask<Void, Void, Boolean> send(Message message);
+
   void onReceive(Function callBack);
+
+  AsyncTask<Void, Void, Boolean> register(Context context);
 }

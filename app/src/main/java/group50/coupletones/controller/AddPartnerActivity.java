b6b3eac1b14ce77293ebdcf09dc9fb5ef1818fe5
joannecho <joannecho96@gmail.com>
2016-05-04 16:25:20 -0700

    Added a button on Settings page to add a partner.

---
+++ app/src/main/java/group50/coupletones/controller/AddPartnerActivity.java
@@ -0,0 +1,16 @@
+package group50.coupletones.controller;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import group50.coupletones.R;
+
+public class AddPartnerActivity extends Activity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_add_partner);
+  }
+
+}

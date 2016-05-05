2e8f895fa37f515f0b616f5ec5236c4e57e4ff0b
sharmaine <hello@sharmaine.me>
2016-05-04 17:32:41 -0700

    Commented code and changed handleReceiver function in GcmManager to void

--- app/src/main/java/group50/coupletones/network/NetworkManager.java
+++ app/src/main/java/group50/coupletones/network/NetworkManager.java
@@ -1,23 +1,57 @@
 package group50.coupletones.network;
 
+/**
+ * @author Sharmaine Manalo
+ * @since 02-05-2016.
+ */
+
 import android.content.Context;
 import android.os.AsyncTask;
+
 import group50.coupletones.network.message.MessageReceiver;
 import group50.coupletones.network.message.OutgoingMessage;
-import group50.coupletones.util.function.Function;
+import group50.coupletones.util.Identifiable;
 
-/**
- * @author sharmaine
- * @since 5/2/16
- */
 public interface NetworkManager {
+  /**
+   * The send function sends an outgoing message to the server.
+   *
+   * @param message - the outgoing message
+   * @return AsyncTask
+   */
   AsyncTask<Void, Void, Boolean> send(OutgoingMessage message);
 
-  void onReceive(Function callBack);
-
+  /**
+   * The register function registers a receiver for each message
+   * sent to the server without having a type specified.
+   *
+   * @param context - the context to register the manager with
+   * @return AsyncTask
+   */
   AsyncTask<Void, Void, Boolean> register(Context context);
 
+  default void register(MessageReceiver receiver) {
+    if (receiver instanceof Identifiable) {
+      register(((Identifiable) receiver).getId(), receiver);
+    } else {
+      throw new RuntimeException("Receiver must implement Identifiable");
+    }
+  }
+
+  /**
+   * The register function registers a receiver for each message
+   * sent to the server.
+   *
+   * @param type     - the type of receiver to register
+   * @param receiver - the receiver to register
+   */
   void register(String type, MessageReceiver receiver);
 
+  /**
+   * The unregister function unregisters a receiver for the type
+   * of receiver passed in.
+   *
+   * @param type - the type of receiver to unregister
+   */
   void unregister(String type);
 }
--- app/src/main/java/group50/coupletones/network/gcm/GcmManager.java
+++ app/src/main/java/group50/coupletones/network/gcm/GcmManager.java
@@ -1,25 +1,28 @@
 package group50.coupletones.network.gcm;
 
+/**
+ * @author Sharmaine Manalo
+ * @since 5/4/16.
+ */
+
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+
 import com.google.android.gms.gcm.GoogleCloudMessaging;
+
 import group50.coupletones.network.NetworkManager;
 import group50.coupletones.network.message.IncomingMessage;
 import group50.coupletones.network.message.MessageReceiver;
 import group50.coupletones.network.message.OutgoingMessage;
 import group50.coupletones.util.Taggable;
-import group50.coupletones.util.function.Function;
 
 import javax.inject.Inject;
+
 import java.io.IOException;
 import java.util.HashMap;
 
-/**
- * @author sharmaine
- * @since 5/4/16
- */
 public class GcmManager implements NetworkManager, Taggable {
   private String PROJECT_NUMBER = "794558589013";
   private GoogleCloudMessaging gcm;
@@ -45,11 +48,6 @@ public class GcmManager implements NetworkManager, Taggable {
         return true;
       }
     }.execute(null, null, null);
-
-  }
-
-  @Override
-  public void onReceive(Function callBack) {
 
   }
 
@@ -95,7 +93,7 @@ public class GcmManager implements NetworkManager, Taggable {
     }
   }
 
-  public void handleReceive(Bundle extras) {
+  void handleReceive(Bundle extras) {
     IncomingMessage msg = new IncomingMessage(extras.getString("type"), extras);
     MessageReceiver messageReceiver = receivers.get(msg.getType());
     messageReceiver.onReceive(msg);

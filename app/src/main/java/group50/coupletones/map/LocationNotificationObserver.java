b18570a34d73445a53f0d9cef1180dd8f560e26b
sharmaine <hello@sharmaine.me>
2016-05-05 16:51:53 -0700

    Added LocationNotificationObserver class

--- app/src/main/java/group50/coupletones/map/LocationNotificationObserver.java
+++ app/src/main/java/group50/coupletones/map/LocationNotificationObserver.java
@@ -1,7 +1,31 @@
 package group50.coupletones.map;
 
+import javax.inject.Inject;
+
+import group50.coupletones.network.NetworkManager;
+import group50.coupletones.network.message.Message;
+import group50.coupletones.network.message.MessageType;
+import group50.coupletones.network.message.OutgoingMessage;
+
 /**
- * Created by sharmaine on 5/5/16.
+ * @author Sharmaine Manalo
+ * @since 5/5/16
  */
-public class LocationNotificationObserver {
+public class LocationNotificationObserver implements NotificationObserver {
+  private NetworkManager network;
+
+  @Inject
+  public LocationNotificationObserver(NetworkManager network) {
+    this.network = network;
+  }
+
+  @Override
+  public void onEnterLocation(VisitedLocation location) {
+    network.send((OutgoingMessage) new OutgoingMessage(MessageType.SEND_LOCATION_NOTIFICATION.value)
+        .setString("name",location.getName())
+        .setDouble("lat", location.getPosition().latitude)
+        .setDouble("long", location.getPosition().longitude)
+    );
+  }
+
 }

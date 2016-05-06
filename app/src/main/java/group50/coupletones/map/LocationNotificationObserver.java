425e9df583d7d208ae3b8d22edc5463b0dca6765
sharmaine <hello@sharmaine.me>
2016-05-05 17:05:39 -0700

    Edited LocationNotificationObserver and LocationNotificationReceiver

--- app/src/main/java/group50/coupletones/map/LocationNotificationObserver.java
+++ app/src/main/java/group50/coupletones/map/LocationNotificationObserver.java
@@ -1,9 +1,12 @@
 package group50.coupletones.map;
 
+import java.text.Format;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import javax.inject.Inject;
 
 import group50.coupletones.network.NetworkManager;
-import group50.coupletones.network.message.Message;
 import group50.coupletones.network.message.MessageType;
 import group50.coupletones.network.message.OutgoingMessage;
 
@@ -21,11 +24,15 @@ public class LocationNotificationObserver implements NotificationObserver {
 
   @Override
   public void onEnterLocation(VisitedLocation location) {
+    Format formatter = new SimpleDateFormat("MM-dd-yyyy");
+    Date date = location.getTime();
+    String time = formatter.format(date);
+
     network.send((OutgoingMessage) new OutgoingMessage(MessageType.SEND_LOCATION_NOTIFICATION.value)
-        .setString("name",location.getName())
-        .setDouble("lat", location.getPosition().latitude)
-        .setDouble("long", location.getPosition().longitude)
+            .setString("name", location.getName())
+            .setDouble("lat", location.getPosition().latitude)
+            .setDouble("long", location.getPosition().longitude)
+            .setString("time", time)
     );
   }
-
 }

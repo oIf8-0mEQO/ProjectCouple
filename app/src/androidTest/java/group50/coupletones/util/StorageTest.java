873b44a714cad5289450d1e43b1edf3a0c54f6a1
Calclavia <admin@calclavia.com>
2016-05-07 14:45:59 -0700

    Added collection test to StorageTest

--- app/src/androidTest/java/group50/coupletones/util/StorageTest.java
+++ app/src/androidTest/java/group50/coupletones/util/StorageTest.java
@@ -2,12 +2,16 @@ package group50.coupletones.util;
 
 import android.content.SharedPreferences;
 import android.support.test.runner.AndroidJUnit4;
+import group50.coupletones.util.storage.Storable;
 import group50.coupletones.util.storage.Storage;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.invocation.InvocationOnMock;
 
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.Assert.assertTrue;
@@ -26,7 +30,7 @@ public class StorageTest {
 
   private HashMap<String, Object> cache;
 
-  @Test
+  @Before
   public void setUp() throws Exception {
     mockPreference = mock(SharedPreferences.class);
 
@@ -42,7 +46,10 @@ public class StorageTest {
 
     // Mck get methods
     when(mockPreference.contains(any())).then(inv -> cache.containsKey(inv));
-    when(mockPreference.getInt(any(), any())).then(this::cacheSet);
+    when(mockPreference.getInt(any(), any())).then(this::cacheGet);
+    when(mockPreference.getBoolean(any(), any())).then(this::cacheGet);
+    when(mockPreference.getFloat(any(), any())).then(this::cacheGet);
+    when(mockPreference.getString(any(), any())).then(this::cacheGet);
 
     storage = new Storage(mockPreference);
   }
@@ -53,7 +60,7 @@ public class StorageTest {
    * @param inv The mock invocation
    */
   private Object cacheSet(InvocationOnMock inv) {
-    cache.put((String) inv.getArguments()[0], inv.getArguments()[1]))
+    cache.put((String) inv.getArguments()[0], inv.getArguments()[1]);
     return null;
   }
 
@@ -64,8 +71,7 @@ public class StorageTest {
    * @param inv The mock invocation
    */
   private Object cacheGet(InvocationOnMock inv) {
-    cache.put((String) inv.getArguments()[0], inv.getArguments()[1]))
-    return null;
+    return cache.get(inv.getArguments()[0]);
   }
 
 
@@ -106,7 +112,38 @@ public class StorageTest {
 
   @Test
   public void testCollection() throws Exception {
-    storage.setCollection();
 
+    class StorableObject implements Storable {
+      public String val;
+
+      public StorableObject() {
+      }
+
+      public StorableObject(String val) {
+        this.val = val;
+      }
+
+      @Override
+      public void save(Storage storage) {
+        storage.setString("test", val);
+      }
+
+      @Override
+      public void load(Storage storage) {
+        val = storage.getString("test");
+      }
+    }
+
+    LinkedList<StorableObject> mockList = new LinkedList<>();
+    mockList.add(new StorableObject("test1"));
+    mockList.add(new StorableObject("test2"));
+    mockList.add(new StorableObject("test3"));
+
+    storage.setCollection("list", mockList);
+
+    List<StorableObject> retrievedList = storage.getCollection("list", StorableObject.class);
+
+    assertThat(retrievedList.size()).isEqualTo(mockList.size());
+    assertThat(retrievedList).isEqualTo(mockList);
   }
 }

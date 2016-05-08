179ccf7a1054191901542740e76fa9e1541213ea
JosephBCox <joseph.vive@gmail.com>
2016-05-08 13:02:30 -0800

    Revert "Fixed the test."
    This reverts commit 5dc0a5cdf43086676a5ecb528f232541428d93da.

--- app/src/androidTest/java/group50/coupletones/controller/tab/favoritelocations/map/MapFragmentTest.java
+++ app/src/androidTest/java/group50/coupletones/controller/tab/favoritelocations/map/MapFragmentTest.java
@@ -2,14 +2,11 @@ package group50.coupletones.controller.tab.favoritelocations.map;
 
 import com.google.android.gms.maps.model.LatLng;
 
-import org.junit.Test;
-
 /**
  * Created by Joseph on 5/8/2016.
  */
 public class MapFragmentTest {
 
-  @Test
   public void testMapMovement()
   {
     MapFragment map = new MapFragment();

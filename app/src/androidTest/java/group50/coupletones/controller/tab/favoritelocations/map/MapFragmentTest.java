873a72a1a359d9af5e465a0a5e0715001db248d0
JosephBCox <joseph.vive@gmail.com>
2016-05-08 13:02:00 -0800

    Changed test.

--- app/src/androidTest/java/group50/coupletones/controller/tab/favoritelocations/map/MapFragmentTest.java
+++ app/src/androidTest/java/group50/coupletones/controller/tab/favoritelocations/map/MapFragmentTest.java
@@ -1,8 +1,12 @@
 package group50.coupletones.controller.tab.favoritelocations.map;
 
+import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 
 import org.junit.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by Joseph on 5/8/2016.
@@ -13,6 +17,8 @@ public class MapFragmentTest {
   public void testMapMovement()
   {
     MapFragment map = new MapFragment();
+    when(map.).
+    map.onMapReady(mock(GoogleMap.class));
     map.moveMap(new LatLng(10, 15));
     assert(map.getMap().getCameraPosition().equals(new LatLng(10, 15)));
   }

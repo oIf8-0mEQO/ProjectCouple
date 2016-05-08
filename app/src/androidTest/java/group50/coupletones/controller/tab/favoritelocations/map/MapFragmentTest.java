eb1c104f074ba8f2c7ef952c249b4af99ea84a69
JosephBCox <joseph.vive@gmail.com>
2016-05-08 13:02:06 -0800

    Revert "Changed test."
    This reverts commit 873a72a1a359d9af5e465a0a5e0715001db248d0.

--- app/src/androidTest/java/group50/coupletones/controller/tab/favoritelocations/map/MapFragmentTest.java
+++ app/src/androidTest/java/group50/coupletones/controller/tab/favoritelocations/map/MapFragmentTest.java
@@ -1,12 +1,8 @@
 package group50.coupletones.controller.tab.favoritelocations.map;
 
-import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.model.LatLng;
 
 import org.junit.Test;
-
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by Joseph on 5/8/2016.
@@ -17,8 +13,6 @@ public class MapFragmentTest {
   public void testMapMovement()
   {
     MapFragment map = new MapFragment();
-    when(map.).
-    map.onMapReady(mock(GoogleMap.class));
     map.moveMap(new LatLng(10, 15));
     assert(map.getMap().getCameraPosition().equals(new LatLng(10, 15)));
   }

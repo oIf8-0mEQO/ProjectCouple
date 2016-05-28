e690aa7c85027aee40667afe9d70bd35c5fe9cd2
sharmaine <hello@sharmaine.me>
2016-05-27 22:08:25 -0700

    Minor fixes

--- app/src/main/java/group50/coupletones/auth/user/User.java
+++ app/src/main/java/group50/coupletones/auth/user/User.java
@@ -6,6 +6,7 @@
 package group50.coupletones.auth.user;
 
 import group50.coupletones.controller.tab.favoritelocations.map.location.FavoriteLocation;
+import group50.coupletones.controller.tab.favoritelocations.map.location.VisitedLocationEvent;
 import group50.coupletones.util.properties.PropertiesProvider;
 import rx.Observable;
 
@@ -36,6 +37,11 @@ public interface User extends PropertiesProvider {
    * @return The list of the users favorite locations.
    */
   List<FavoriteLocation> getFavoriteLocations();
+
+  /**
+   * @return The list of the user's visited locations.
+   */
+  List<VisitedLocationEvent> getVisitedLocations();
 
   /**
    * @return An observable that completes after user is fully loaded from database.

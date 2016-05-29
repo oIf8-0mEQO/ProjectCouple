30df9ae650c5b1a5acd0294b3949002eba64fa38
sharmaine <hello@sharmaine.me>
2016-05-28 20:07:14 -0700

    Fixed issue with disconnect button not being displayed, but needs to be looked into as to why performing click on disconnect button via Espresso does not disconnect partners.

--- app/src/androidTest/java/group50/coupletones/bdd/UserDeletesPartner.java
+++ app/src/androidTest/java/group50/coupletones/bdd/UserDeletesPartner.java
@@ -1,5 +1,6 @@
 package group50.coupletones.bdd;
 
+import android.support.test.espresso.action.ViewActions;
 import android.support.test.espresso.assertion.ViewAssertions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -15,6 +16,7 @@ import org.junit.runner.RunWith;
 
 import group50.coupletones.CoupleTones;
 import group50.coupletones.R;
+import group50.coupletones.UserMocker;
 import group50.coupletones.auth.user.LocalUser;
 import group50.coupletones.auth.user.Partner;
 import group50.coupletones.controller.MainActivity;
@@ -26,8 +28,10 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.isNull;
 import static org.mockito.Matchers.matches;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
@@ -49,14 +53,6 @@ public class UserDeletesPartner {
   private LocalUser mockUser;
   private Partner partner;
 
-  /**
-   * User removes partner
-   * Given that the user has a partner
-   * When the the user clicks on "Remove Partner" on the Settings Page
-   * Then the user's partner is removed
-   * And the user is removed as the partner's partner.
-   */
-
   @Before
   public void setup() {
     // Mock DI
@@ -75,6 +71,15 @@ public class UserDeletesPartner {
     when(app.isLoggedIn()).thenReturn(true);
     when(app.getLocalUser()).thenReturn(mockUser);
     when(mockUser.getPartner()).thenReturn(Observable.just(partner));
+
+    new UserMocker(mockUser)
+        .mockProperty("name", "sharmaine")
+        .mockProperty("email", "sharmaine@domain.com");
+
+
+    new UserMocker(partner)
+        .mockProperty("name", "henry")
+        .mockProperty("email", "henry@domain.com");
   }
 
   private void givenThatTheUserHasPartner() {
@@ -82,11 +87,17 @@ public class UserDeletesPartner {
   }
 
   private void whenUserClicksRemovePartnerOnSettingsPage() {
-    onView(withId(R.id.favorite_locations)).perform(click());
+    onView(withId(R.id.settings)).perform(click());
+
+    // TODO: Clicking disconnect not working
+    onView(withId(R.id.disconnect_button))
+        .perform(ViewActions.scrollTo())
+        .perform(click());
   }
 
   private void thenUserPartnerIsRemoved() {
-    onView(withId(R.id.partner_locations)).check(ViewAssertions.matches(isDisplayed()));
+    // Verify that "No Partner" is displayed on Partner Profile card
+    //onView(withId(R.id.null_partner)).check(ViewAssertions.matches(isDisplayed()));
   }
 
   private void andUserIsRemovedAsPartnersPartner() {
@@ -101,7 +112,7 @@ public class UserDeletesPartner {
   public void userDeletesPartner() {
     givenThatTheUserHasPartner();
     whenUserClicksRemovePartnerOnSettingsPage();
-    //thenUserPartnerIsRemoved();
+    thenUserPartnerIsRemoved();
     //andUserIsRemovedAsPartnersPartner();
   }
 }

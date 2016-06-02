dacb9465627ad1d6927486ae684e7f2b61a90c17
Calclavia <admin@calclavia.com>
2016-06-01 23:05:13 -0700

    Integrated Firebase Cloud Messaging

---
+++ app/src/main/java/group50/coupletones/network/fcm/InstanceIDService.java
@@ -0,0 +1,22 @@
+package group50.coupletones.network.fcm;
+
+import android.util.Log;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+import group50.coupletones.util.Taggable;
+
+/**
+ * FCM ID refresher
+ * @author Henry Mao
+ */
+public class InstanceIDService extends FirebaseInstanceIdService implements Taggable {
+
+  @Override
+  public void onTokenRefresh() {
+    // Get updated InstanceID token.
+    String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+    Log.d(getTag(), "Refreshed token: " + refreshedToken);
+
+    // TODO: Implement this method to send any registration to your app's servers.
+  }
+}
---
+++ app/src/main/java/group50/coupletones/network/fcm/MessagingService.java
@@ -0,0 +1,11 @@
+package group50.coupletones.network.fcm;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+/**
+ * FCM service
+ * @author Henry Mao
+ */
+public class MessagingService extends FirebaseMessagingService {
+
+}

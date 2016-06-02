bb0cfc8562dc847d38146e94c9a69ae0da58b938
JosephBCox <joseph.vive@gmail.com>
2016-06-02 15:08:10 -0800

    VibeTone names and getTones method.

--- app/src/main/java/group50/coupletones/util/sound/VibeTone.java
+++ app/src/main/java/group50/coupletones/util/sound/VibeTone.java
@@ -9,6 +9,11 @@ import android.net.Uri;
 import android.os.Vibrator;
 
 import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -24,18 +29,18 @@ import com.google.firebase.database.IgnoreExtraProperties;
 public class VibeTone {
 
   private static VibeTone[] tones = {
-    new VibeTone("/assets/tones/vibetone1.mps", new long[] {0, 2000}),//Index 0 / default tone.
-    new VibeTone("/assets/tones/vibetone2.mps", new long[] {0, 400, 400, 400, 400, 400}),//Index 1.
-    new VibeTone("/assets/tones/vibetone3.mps", new long[] {0, 800, 400, 800}),//Index 2.
-    new VibeTone("/assets/tones/vibetone4.mps", new long[] {0, 1200, 400, 400}),//Index 3.
-    new VibeTone("/assets/tones/vibetone5.mps", new long[] {0, 400, 400, 1200}),//Index 4.
-    new VibeTone("/assets/tones/vibetone6.mps", new long[] {0, 800, 400, 200, 400, 200}),//Index 5.
-    new VibeTone("/assets/tones/vibetone7.mps", new long[] {0, 200, 400, 800, 400, 200}),//Index 6.
-    new VibeTone("/assets/tones/vibetone8.mps", new long[] {0, 200, 400, 200, 400, 800}),//Index 7.
-    new VibeTone("/assets/tones/vibetone9.mps", new long[] {0, 800, 400, 800, 400, 800}),//Index 8.
-    new VibeTone("/assets/tones/vibetone10.mps", new long[] {0, 800, 400, 800, 400, 800, 400, 800}),//Index 9.
-    new VibeTone("/assets/tones/arrivaltone.mp3", new long[] {0, 1000}),//Index 10 / Arrival.
-    new VibeTone("/assets/tones/departuretone.mps", new long[] {0, 400, 200, 400}),//Index 11 / Departure.
+    new VibeTone("/assets/tones/vibetone1.mps", new long[] {0, 2000}, "Pikachu"),//Index 0 / default tone.
+    new VibeTone("/assets/tones/vibetone2.mps", new long[] {0, 400, 400, 400, 400, 400}, "Coin"),//Index 1.
+    new VibeTone("/assets/tones/vibetone3.mps", new long[] {0, 800, 400, 800}, "Super Mario"),//Index 2.
+    new VibeTone("/assets/tones/vibetone4.mps", new long[] {0, 1200, 400, 400}, "Pokemon Battle"),//Index 3.
+    new VibeTone("/assets/tones/vibetone5.mps", new long[] {0, 400, 400, 1200}, "Kim Possible"),//Index 4.
+    new VibeTone("/assets/tones/vibetone6.mps", new long[] {0, 800, 400, 200, 400, 200}, "Kakao"),//Index 5.
+    new VibeTone("/assets/tones/vibetone7.mps", new long[] {0, 200, 400, 800, 400, 200}, "Bling"),//Index 6.
+    new VibeTone("/assets/tones/vibetone8.mps", new long[] {0, 200, 400, 200, 400, 800}, "1 Up"),//Index 7.
+    new VibeTone("/assets/tones/vibetone9.mps", new long[] {0, 800, 400, 800, 400, 800}, "Windows"),//Index 8.
+    new VibeTone("/assets/tones/vibetone10.mps", new long[] {0, 800, 400, 800, 400, 800, 400, 800}, "Zelda"),//Index 9.
+    new VibeTone("/assets/tones/arrivaltone.mp3", new long[] {0, 1000}, "Arrival"),//Index 10 / Arrival.
+    new VibeTone("/assets/tones/departuretone.mps", new long[] {0, 400, 200, 400}, "Departure"),//Index 11 / Departure.
   };
 
   public static final int MAX_VIBETONE_COUNT = 10;
@@ -45,6 +50,7 @@ public class VibeTone {
 
   private Ringtone sound;
   private long[] vibration;
+  private String name;
 
   private static long delay = 1500;//The amount of time in milliseconds to wait between arrival/departure global sound and specific sound.
 
@@ -66,9 +72,15 @@ public class VibeTone {
     return tones[which];
   }
 
-  private VibeTone(String filePath, long[] vibration)
+  public static List<VibeTone> getVibeTones()
+  {
+    return Collections.unmodifiableList(Arrays.asList());
+  }
+
+  private VibeTone(String filePath, long[] vibration, String name)
   {
     this.vibration = vibration;
+    this.name = name;
     Uri file = Uri.fromFile(new File(filePath));
     RingtoneManager.getRingtone(app.getApplicationContext(), file);
   }
@@ -105,6 +117,11 @@ public class VibeTone {
     {
       System.out.println(e.getMessage());
     }
+  }
+
+  public String getName()
+  {
+    return name;
   }
 
   public int getIndex()

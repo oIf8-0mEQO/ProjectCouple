a66d7d448f55e0ec3cc4cd27abe2cc12cd107fe7
Calclavia <admin@calclavia.com>
2016-06-01 18:18:28 -0700

    Finished task 113 BDD test and fixed clearing history edge cases

--- app/src/main/java/group50/coupletones/util/TimeUtility.java
+++ app/src/main/java/group50/coupletones/util/TimeUtility.java
@@ -1,9 +1,9 @@
 package group50.coupletones.util;
 
+import group50.coupletones.controller.tab.favoritelocations.map.location.VisitedLocationEvent;
+
 import java.util.Calendar;
 import java.util.Date;
-
-import group50.coupletones.controller.tab.favoritelocations.map.location.VisitedLocationEvent;
 
 /**
  * @author Sharmaine Manalo
@@ -22,31 +22,32 @@ public class TimeUtility {
    * @return System time
    */
   public Date systemDate() {
-    Calendar c = Calendar.getInstance();
-    return c.getTime();
+    return new Date(systemTime());
   }
 
   /**
    * This function checks to see if the time of the visited location is before 3AM
+   *
    * @param visitedLocation The location to check the time of
    * @return true if visited location time is before 3AM, otherwise false
    */
-  public boolean checkTime(VisitedLocationEvent visitedLocation) {
-
+  public boolean isFromPreviousDay(VisitedLocationEvent visitedLocation) {
     // Get the time visited of visited location
-    Date currentTime = visitedLocation.getTimeVisited();
+    Date visitTime = visitedLocation.getTimeVisited();
 
     // Refer to 3AM of current day
-    Calendar reset = Calendar.getInstance();
-    reset.set(Calendar.HOUR_OF_DAY, 3);
-    reset.set(Calendar.MINUTE, 00);
-    Date resetTime = reset.getTime();
+    Calendar resetTime = Calendar.getInstance();
+    resetTime.setTime(systemDate());
 
-    boolean b = false;
-
-    if (currentTime.after(resetTime)) {
-      b = true;
+    // If it's between 0 AM and 3AM, backtrack one day.
+    if (resetTime.get(Calendar.HOUR_OF_DAY) < 3) {
+      resetTime.set(Calendar.DAY_OF_YEAR, resetTime.get(Calendar.DAY_OF_YEAR) - 1);
     }
-    return b;
+
+    // Set to 3AM of the current day
+    resetTime.set(Calendar.HOUR_OF_DAY, 3);
+    resetTime.set(Calendar.MINUTE, 0);
+    resetTime.set(Calendar.SECOND, 0);
+    return visitTime.before(resetTime.getTime());
   }
 }

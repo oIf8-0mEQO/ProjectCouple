81b734b0e5770d951979ce7cc718aa6d1bf8f6b4
JosephBCox <joseph.vive@gmail.com>
2016-06-02 14:13:30 -0800

    Started location dragging.

---
+++ app/src/main/java/group50/coupletones/controller/tab/favoritelocations/map/LocationDragMediator.java
@@ -0,0 +1,16 @@
+package group50.coupletones.controller.tab.favoritelocations.map;
+
+import com.google.android.gms.maps.GoogleMap;
+
+import group50.coupletones.controller.tab.favoritelocations.map.location.FavoriteLocation;
+
+/**
+ * Manages changes between a marker and the Favorite Location it represents.
+ * @author joseph
+ * @since 6/2/16
+ */
+public class LocationDragMediator implements GoogleMap.OnMarkerDragListener {
+
+  private FavoriteLocation loc
+
+}

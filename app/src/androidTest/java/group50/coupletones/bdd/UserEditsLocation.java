ec0c43e0844935b69c36098fcb64cceb96a3432e
sharmaine <hello@sharmaine.me>
2016-06-02 22:24:48 -0700

    Fixed UserEditsLocation BDD test

--- app/src/androidTest/java/group50/coupletones/bdd/UserEditsLocation.java
+++ app/src/androidTest/java/group50/coupletones/bdd/UserEditsLocation.java
@@ -30,20 +30,26 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withChild;
+import static android.support.test.espresso.matcher.ViewMatchers.withClassName;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 
+import static org.assertj.core.api.Assertions.allOf;
 import static org.assertj.core.api.Assertions.assertThat;
 
 
 /**
- * Created by Joseph on 6/2/2016.
+ * @author Joseph Cox
+ * @since 6/2/2016
  */
-public class UserRenamesFavoriteLocation {
+public class UserEditsLocation {
 
   private FavoriteLocation location;
   private UserTestUtil userTestUtil;
+  LinkedList<FavoriteLocation> list;
   private LocalUser user;
   private CoupleTones app;
 
@@ -60,34 +66,33 @@ public class UserRenamesFavoriteLocation {
                            .injectLocalUser()
                            .get();
     }
-
-    @Override
-    protected void afterActivityLaunched() {
-      super.afterActivityLaunched();
-    }
   };
 
   @Before
   public void setup() {
-    LinkedList<FavoriteLocation> list = new LinkedList<>();
-    userTestUtil.mockFavoriteLocations(() -> list);
+    list = new LinkedList<>();
+    location = new FavoriteLocation("Test", new LatLng(0, 0), 0, 0);
+    list.add(location);
+    userTestUtil.get().getProperties()
+      .property("favoriteLocations")
+      .set(list);
   }
 
   public void givenUserHasAFavoriteLocation() {
-    location = new FavoriteLocation("test name", new LatLng(10, 10), 0, VibeTone.getTone(1));
-    user.addFavoriteLocation(location);
+    assertThat(list).hasSize(1);
   }
 
   public void whenUserInputsNewName() throws Throwable {
     onView(withId(R.id.favorite_locations)).perform(click());
     onView(withId(R.id.edit_location_icon)).perform(click());
-    onView(withId(R.id.location_name)).perform(typeText("qwerty"));
+    onView(withId(R.id.location_name)).perform(click()).perform(typeText("qwerty"));
     Espresso.closeSoftKeyboard();
     onView(withId(R.id.save_changes_button)).perform(click());
   }
 
   public void thenLocationsNameWillChange() throws Throwable {
-    onView(withId(R.id.list_item_name)).check(matches(withText("qwerty")));
+    onView(withId(R.id.favorite_locations)).check(matches(isDisplayed()));
+    onView(withId(R.id.list_item_name)).check(matches(withText("Testqwerty")));
   }
 
   @Test

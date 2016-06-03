e6562f81cfe2f8ee021de22685d877641946a7fa
JosephBCox <joseph.vive@gmail.com>
2016-06-02 16:25:54 -0800

    Started tests.

---
+++ app/src/androidTest/java/group50/coupletones/bdd/UserDragsFavoriteLocationMarker.java
@@ -0,0 +1,16 @@
+package group50.coupletones.bdd;
+
+import org.junit.Test;
+
+/**
+ * Created by Joseph on 6/2/2016.
+ */
+public class UserDragsFavoriteLocationMarker {
+
+  @Test
+  public void userDragsMarker()
+  {
+    given
+  }
+
+}

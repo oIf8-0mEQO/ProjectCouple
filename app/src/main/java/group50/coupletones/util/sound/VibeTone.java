4d0c4a1f2219e4c23d6e60d75523955cccaf9f10
sharmaine <hello@sharmaine.me>
2016-06-03 22:47:34 -0700

    New branch for demo

--- app/src/main/java/group50/coupletones/util/sound/VibeTone.java
+++ app/src/main/java/group50/coupletones/util/sound/VibeTone.java
@@ -1,24 +1,21 @@
 package group50.coupletones.util.sound;
-
 import android.content.Context;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Vibrator;
 import group50.coupletones.CoupleTones;
-
+import group50.coupletones.R;
 import javax.inject.Inject;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-
 /**
  * @Author Joseph
  * @Since 5/21/16
  */
 public class VibeTone {
-
   public static final int MAX_VIBETONE_COUNT = 10;
   public static final int ARRIVAL_VIBETONE = 10;
   public static final int DEPARTURE_VIBETONE = 11;
@@ -29,39 +26,41 @@ public class VibeTone {
   private Ringtone sound;
   private long[] vibration;
   private String name;
-
-  private VibeTone(String fileName, long[] vibration, String name) {
+  private Uri arrivalFile, departureFile;
+  private Ringtone arrivalTone, departureTone;
+  private VibeTone(int resource, long[] vibration, String name) {
     CoupleTones.global().inject(this);
     this.vibration = vibration;
     this.name = name;
-    Uri file = Uri.parse("android.resource://group50.coupletones/raw/tones" + fileName);
+    Uri file = Uri.parse("android.resource://" + app.getApplicationContext().getPackageName() + "/" + resource);
+    arrivalFile = Uri.parse("android.resource://" + app.getApplicationContext().getPackageName() + "/" + R.raw.arrivaltone);
+    departureFile = Uri.parse("android.resource://" + app.getApplicationContext().getPackageName() + "/" + R.raw.departuretone);
     sound = RingtoneManager.getRingtone(app.getApplicationContext(), file);
+    arrivalTone = RingtoneManager.getRingtone(app.getApplicationContext(), arrivalFile);
+    departureTone = RingtoneManager.getRingtone(app.getApplicationContext(), departureFile);
   }
-
   public static void loadTones() {
     tones = new VibeTone[]{
-      new VibeTone("vibetone1.mp3", new long[]{0, 2000}, "Pikachu"),//Index 0 / default tone.
-      new VibeTone("vibetone2.mp3", new long[]{0, 400, 400, 400, 400, 400}, "Coin"),//Index 1.
-      new VibeTone("vibetone3.mp3", new long[]{0, 800, 400, 800}, "Super Mario"),//Index 2.
-      new VibeTone("vibetone4.mp3", new long[]{0, 1200, 400, 400}, "Pokemon Battle"),//Index 3.
-      new VibeTone("vibetone5.mp3", new long[]{0, 400, 400, 1200}, "Kim Possible"),//Index 4.
-      new VibeTone("vibetone6.mp3", new long[]{0, 800, 400, 200, 400, 200}, "Kakao"),//Index 5.
-      new VibeTone("vibetone7.mp3", new long[]{0, 200, 400, 800, 400, 200}, "Bling"),//Index 6.
-      new VibeTone("vibetone8.mp3", new long[]{0, 200, 400, 200, 400, 800}, "1 Up"),//Index 7.
-      new VibeTone("vibetone9.mp3", new long[]{0, 800, 400, 800, 400, 800}, "Windows"),//Index 8.
-      new VibeTone("vibetone10.mp3", new long[]{0, 800, 400, 800, 400, 800, 400, 800}, "Zelda"),//Index 9.
-      new VibeTone("arrivaltone.mp3", new long[]{0, 1000}, "Arrival"),//Index 10 / Arrival.
-      new VibeTone("departuretone.mp3", new long[]{0, 400, 200, 400}, "Departure"),//Index 11 / Departure.
+      new VibeTone(R.raw.vibetone1, new long[]{0, 2000}, "Pikachu"),//Index 0 / default tone.
+      new VibeTone(R.raw.vibetone2, new long[]{0, 400, 400, 400, 400, 400}, "Coin"),//Index 1.
+      new VibeTone(R.raw.vibetone3, new long[]{0, 800, 400, 800}, "Super Mario"),//Index 2.
+      new VibeTone(R.raw.vibetone4, new long[]{0, 1200, 400, 400}, "Pokemon Battle"),//Index 3.
+      new VibeTone(R.raw.vibetone5, new long[]{0, 400, 400, 1200}, "Kim Possible"),//Index 4.
+      new VibeTone(R.raw.vibetone6, new long[]{0, 800, 400, 200, 400, 200}, "Kakao"),//Index 5.
+      new VibeTone(R.raw.vibetone7, new long[]{0, 200, 400, 800, 400, 200}, "Bling"),//Index 6.
+      new VibeTone(R.raw.vibetone8, new long[]{0, 200, 400, 200, 400, 800}, "1 Up"),//Index 7.
+      new VibeTone(R.raw.vibetone9, new long[]{0, 800, 400, 800, 400, 800}, "Windows"),//Index 8.
+      new VibeTone(R.raw.vibetone10, new long[]{0, 800, 400, 800, 400, 800, 400, 800}, "Zelda"),//Index 9.
+      new VibeTone(R.raw.arrivaltone, new long[]{0, 1000}, "Arrival"),//Index 10 / Arrival.
+      new VibeTone(R.raw.departuretone, new long[]{0, 400, 200, 400}, "Departure"),//Index 11 / Departure.
     };
   }
-
   /**
    * @return the default vibetone.
    */
   public static VibeTone getDefaultTone() {
     return tones[0];
   }
-
   /**
    * @return the ith vibetone.
    */
@@ -69,67 +68,56 @@ public class VibeTone {
     if (which < 0 || which >= MAX_VIBETONE_COUNT) return tones[0];
     return tones[which];
   }
-
   public static List<VibeTone> getVibeTones() {
     List<VibeTone> availableTones = new LinkedList<>();
     for (int i = 0; i < MAX_VIBETONE_COUNT; i++)
       availableTones.add(tones[i]);
-
     return Collections.unmodifiableList(availableTones);
   }
-
   public void playArrival() {
     try {
-      tones[ARRIVAL_VIBETONE].play();
+      arrivalTone.play();
       Thread.sleep(delay);
       play();
     } catch (Exception e) {
       System.out.println(e.getMessage());
     }
   }
-
   public void playDeparture() {
     try {
-      tones[DEPARTURE_VIBETONE].play();
+      departureTone.play();
       Thread.sleep(delay);
       play();
     } catch (Exception e) {
       System.out.println(e.getMessage());
     }
   }
-
   private void play() {
     if (app.getLocalUser().getVibrationSetting())
       playVibrate();
     if (app.getLocalUser().getTonesSetting())
       playSound();
   }
-
   public void playVibrate() {
     Vibrator vib = (Vibrator) app.getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
     vib.vibrate(vibration, 0);
   }
-
   public void playSound() {
     sound.play();
   }
-
   public String getName() {
     return name;
   }
-
   public int getIndex() {
     for (int i = 0; i < tones.length; i++) {
       if (tones[i].equals(this)) return i;
     }
     throw new RuntimeException("VibeTones should only exist as one of the elements contained in tones.");
   }
-
   @Override
   public int hashCode() {
     return getIndex();
   }
-
   @Override
   public boolean equals(Object object) {
     if (object instanceof VibeTone) {
@@ -143,5 +131,4 @@ public class VibeTone {
     }
     return false;
   }
-
 }
